cc5e6727391824eff821220cca6e9a2f3866b30d
yuki_sdy <couragewing.14@gmail.com>
2022-12-07 15:16:19 +0900

    appファイル名変更

---
+++ frontend/src/App.tsx
@@ -0,0 +1,81 @@
+import React, { useState, useEffect, createContext } from "react"
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom"
+
+import CommonLayout from "./components/layout/CommonLayout"
+import Home from "./components/pages/Home"
+import SignIn from "./components/pages/SignIn"
+import SignUp from "./components/pages/SignUp"
+
+import { User } from "./interfaces"
+import { getCurrentUser } from "./lib/api/auth"
+
+// グローバルで扱う変数・関数
+export const AuthContext = createContext({} as {
+  loading: boolean
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>
+  isSignedIn: boolean
+  setIsSignedIn: React.Dispatch<React.SetStateAction<boolean>>
+  currentUser: User | undefined
+  setCurrentUser: React.Dispatch<React.SetStateAction<User | undefined>>
+})
+
+const App: React.FC = () => {
+  const [loading, setLoading] = useState<boolean>(true)
+  const [isSignedIn, setIsSignedIn] = useState<boolean>(false)
+  const [currentUser, setCurrentUser] = useState<User | undefined>()
+
+  // 認証済みのユーザーがいるかどうかチェック
+  // 確認できた場合はそのユーザーの情報を取得
+  const handleGetCurrentUser = async () => {
+    try {
+      const res = await getCurrentUser()
+      console.log(res)
+
+      if (res?.status === 200) {
+        setIsSignedIn(true)
+        setCurrentUser(res?.data.currentUser)
+      } else {
+        console.log("No current user")
+      }
+    } catch (err) {
+      console.log(err)
+    }
+
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    handleGetCurrentUser()
+  }, [setCurrentUser])
+
+
+  // ユーザーが認証済みかどうかでルーティングを決定
+  // 未認証だった場合は「/signin」ページに促す
+  const Private = ({ children }: { children: React.ReactElement }) => {
+    if (!loading) {
+      if (isSignedIn) {
+        return children
+      } else {
+        return <Navigate to="/signin" />
+      }
+    } else {
+      return <></>
+    }
+  }
+
+  return (
+    <Router>
+      <AuthContext.Provider value={{ loading, setLoading, isSignedIn, setIsSignedIn, currentUser, setCurrentUser}}>
+        <CommonLayout>
+          <Routes>
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/" element={<Private children={<Home />} />} />
+          </Routes>
+        </CommonLayout>
+      </AuthContext.Provider>
+    </Router>
+  )
+}
+
+export default App
