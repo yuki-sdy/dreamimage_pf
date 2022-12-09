018ae3bc444c40e4c16226378201c3b36bc41214
yuki_sdy <couragewing.14@gmail.com>
2022-12-09 11:11:30 +0900

    usercontroller手直し

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::UsersController < ApplicationController
   def destroy
     user = User.find(params[:id])
     user.destroy
-    render json: { status: 200, message: 'Delete the user'}
+    render json: { status: 200 }
   end
 end
