3061f33661e6f6b417d226326211044ff49d248d
yuki_sdy <couragewing.14@gmail.com>
2022-12-09 11:08:19 +0900

    DreamDiaryテーブルの作成

---
+++ db/migrate/20221207133005_create_dream_diary.rb
@@ -0,0 +1,18 @@
+class CreateDreamDiary < ActiveRecord::Migration[6.1]
+  def change
+    create_table :dream_diaries do |t|
+      t.string :title
+      t.string :body
+      t.string :prompt
+      t.date :dream_date
+      t.integer :impression, null: false, default: 0
+      t.integer :dream_type, null: false, default: 0
+      t.integer :state, null: false, default: 0
+      t.text :image
+      t.string :daiary_ogp
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_05_091145) do
+ActiveRecord::Schema.define(version: 2022_12_07_133005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dream_diaries", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.string "prompt"
+    t.date "dream_date"
+    t.integer "impression", default: 0, null: false
+    t.integer "dream_type", default: 0, null: false
+    t.integer "state", default: 0, null: false
+    t.text "image"
+    t.string "daiary_ogp"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_dream_diaries_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -40,4 +56,5 @@ ActiveRecord::Schema.define(version: 2022_12_05_091145) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "dream_diaries", "users"
 end
