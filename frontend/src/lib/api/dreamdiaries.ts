a9852641c11da8f557bb8cba235b16f5a8b01205
yuki_sdy <couragewing.14@gmail.com>
2022-12-10 14:20:19 +0900

    create, update, destroyのための関数追記

--- frontend/src/lib/api/dreamdiaries.ts
+++ frontend/src/lib/api/dreamdiaries.ts
@@ -1,6 +1,8 @@
 import { client } from "./client"
 import Cookies from "js-cookie"
+import { DreamDiaryFormData } from "../../interfaces"
 
+// indexへのgetリクエスト、全件取得
 export const getDreamDiaries = () => {
   return client.get("dream_diaries", { headers: {
     "access-token": Cookies.get("_access_token"),
@@ -10,6 +12,7 @@ export const getDreamDiaries = () => {
   })
 }
 
+// showとeditへのgetリクエスト、idに該当する1件取得
 export const getDreamDiary = (id: number) => {
   return client.get(`dream_diaries/${id}`, { headers: {
     "access-token": Cookies.get("_access_token"),
@@ -17,4 +20,23 @@ export const getDreamDiary = (id: number) => {
     "uid": Cookies.get("_uid")
   }
   })
+}
+
+//createへのpostリクエスト、newから
+export const DreamDiaryCreate = (data: DreamDiaryFormData) => {
+  return client.post("dream_diaries", data )
+}
+
+// updateへのputリクエスト、editから
+export const DreamDiaryUpdate = (id: number, data: DreamDiaryFormData) => {
+  return client.put(`dream_diaries/${id}`, data)
+}
+
+// destroyへのdeleteリクエスト
+export const DreamDiaryDestroy = (id: number | undefined) => {
+  return client.delete(`dream_diaries/${id}`, { headers: {
+    "access-token": Cookies.get("_access_token"),
+    "client": Cookies.get("_client"),
+    "uid": Cookies.get("_uid")
+  }})  
 }
