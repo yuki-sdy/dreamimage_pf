135a1ddcf3de5a49fe7d759a0d5dc95878150d09
yuki_sdy <couragewing.14@gmail.com>
2023-01-01 19:36:44 +0900

    回数制限微調整

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -63,9 +63,9 @@ class Api::V1::ImagesController < ApplicationController
     end
 
     if current_box.user_type == 0
-      current_box.images.count >= 3 ? current_box.update(limit: true) : current_box.update(limit: false)
+      current_box.images.count >= 30 ? current_box.update(limit: true) : current_box.update(limit: false)
     else
-      current_box.images.count >= 5 ? current_box.update(limit: true) : current_box.update(limit: false)
+      current_box.images.count >= 50 ? current_box.update(limit: true) : current_box.update(limit: false)
     end
     current_box
   end
