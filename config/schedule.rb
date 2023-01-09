02b71f1e35df3a67f51b79a3c65c03cf9ff638b8
yuki_sdy <couragewing.14@gmail.com>
2023-01-09 12:49:44 +0900

    schedule.rb修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,9 +3,10 @@ require File.expand_path(File.dirname(__FILE__) + '/environment')
 set      :job_template, "/bin/zsh -l -c ':job'"
 job_type :rake, "source /Users/yanagisawayuki/.zshrc; export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output"
 
-rails_env = ENV['RAILS_ENV'] || :development
+rails_env = ENV['RAILS_ENV'] ||= :production
 set :environment, rails_env
 set :output, "#{Rails.root}/log/cron.log"
+ENV.each { |k, v| env(k, v) }
 
 # every :day, at: '0:00 am' do
 every 1.minutes do
