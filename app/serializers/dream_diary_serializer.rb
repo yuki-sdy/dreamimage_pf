df06f820ec8c183598a7bc6e998b08ade86a1191
yuki_sdy <couragewing.14@gmail.com>
2023-01-12 21:30:17 +0900

    serializerの追加や修正

--- app/serializers/dream_diary_serializer.rb
+++ app/serializers/dream_diary_serializer.rb
@@ -1,5 +1,6 @@
 class DreamDiarySerializer < ActiveModel::Serializer
-  attributes :id, :title, :image, :content, :dream_date, :impression, :dream_type
+  attributes :id, :title, :image, :content, :dream_date, :impression, :dream_type, :diary_ogp, :user_id
 
   belongs_to :user
+  has_many :likes
 end
