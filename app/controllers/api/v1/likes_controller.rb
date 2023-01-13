6db76ec49cfbcc5cc5e419bcbc6efba027e8cae9
yuki_sdy <couragewing.14@gmail.com>
2023-01-13 13:42:17 +0900

    likesコントローラーとAPI関数の追加

---
+++ app/controllers/api/v1/likes_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::LikesController < ApplicationController
+  before_action :set_like_attributes, only: %i[create]
+
+  def create
+    like = @current_user.like(@dream_diary)
+    render json: { status: 200, like: like }
+  end
+  
+  def destroy
+    @dream_diary = DreamDiary.find(like_params[:dream_diary_id])
+    current_api_v1_user.unlike(@dream_diary)
+    likes = @dream_diary.likes
+    render json: { status: 200, likes: likes }
+  end
+  
+  private
+  
+  def like_params
+    params.permit(:user_id, :dream_diary_id)
+  end
+  
+  def set_like_attributes
+    @current_user = User.find(like_params[:user_id])
+    @dream_diary = DreamDiary.find(like_params[:dream_diary_id])
+  end
+end
