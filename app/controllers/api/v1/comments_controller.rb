cb27237e37080450d7b8c3f8aa432889719ee8e5
yuki_sdy <couragewing.14@gmail.com>
2023-01-19 12:34:38 +0900

    comments_controllerを追加

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,27 @@
+class Api::V1::CommentsController < ApplicationController
+  before_action :set_comment_attributes, only: %i[create]
+
+  def create
+    comment = @current_user.comments.new(comment_params)
+    if comment.save
+      render json: comment, include: [:user], status: 200
+    end
+  end
+
+  def destroy
+    comment = current_api_v1_user.comments.find(params[:id])
+    comment.destroy
+    render json: { status: 200 }
+  end
+
+  private
+
+  def comment_params
+    params.permit(:user_id, :dream_diary_id, :body)
+  end
+
+  def set_comment_attributes
+    @current_user = User.find(comment_params[:user_id])
+    @dream_diary = DreamDiary.find(comment_params[:dream_diary_id])
+  end
+end
