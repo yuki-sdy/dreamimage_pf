d80c043818a5523c610fd4c3e7dd82c6a8b4f7fa
yuki_sdy <couragewing.14@gmail.com>
2023-01-24 06:46:06 +0900

    passwords_controllerとルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
 
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         registrations: 'api/v1/auth/registrations',
+        passwords: 'api/v1/auth/passwords'
         # omniauth_callbacks: 'api/v1/auth/omniauth_callbacks',
       }
       namespace :auth do
